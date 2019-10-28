b88d51120724347b90e10f2c6adb51098ca62a50
Phil Billingsby <philbillingsby@gmail.com>
2019-10-28 13:27:07 -0400

    Done.

---
+++ .history/lib/book_20191028131914.rb
@@ -0,0 +1,6 @@
+class Book
+  GENRES = []
+  def initialize(name)
+    @name = name
+    GENRES << name
+end
---
+++ .history/lib/shoe_20191028132509.rb
@@ -0,0 +1,6 @@
+class Shoe
+  BRANDS = []
+  def initialize(brand)
+    @brand = brand
+    BRANDS << @brand
+end
